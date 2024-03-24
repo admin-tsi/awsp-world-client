8c6154e8f8f3756bb92f7fc34ca253c71e08407c
Bitsmith01 <esperat.dimon@epitech.eu>
2024-03-24 20:50:43 +0100

    [set up]: restriction on microcredential that was not paid by user

---
+++ src/components/quizIncompleteWarning.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+function QuizIncompleteWarning({}: Props) {
+  return (
+    <div className="h-full w-full absolute z-50">
+      <span>Notice</span>
+    </div>
+  );
+}
+
+export default QuizIncompleteWarning;
